061f2e1dcb80f41e4771ad5b929f38ca26b8cff3
Matthew Fogarty <mattempyre@gmail.com>
2022-10-28 22:37:55 +0200

    Setup Auth. Login Page. Logout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 import Row from '../components/Row';
+import useAuth from '../hooks/useAuth';
 import { Movie } from '../typings';
 import requests from '../utils/requests';
 
@@ -27,7 +28,10 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
-  console.log(netflixOriginals);
+  const { loading } = useAuth();
+
+  if (loading) return null;
+
   return (
     <div className='relative h-screen bg-gradient-to-b lg:h-[140vh]'>
       <Head>
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,32 @@
+import { signInWithPopup } from 'firebase/auth';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import useAuth from '../hooks/useAuth';
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 function Login() {
+  const [login, setLogin] = useState(false);
+  const { signIn, signUp } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (login) {
+      await signIn(email, password);
+    } else {
+      await signUp(email, password);
+    }
+  };
+
   return (
     <div className='relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent'>
       <Head>
@@ -20,17 +45,44 @@ function Login() {
         width={150}
         height={150}
       />
-      <form className='relative mt-24 space-y-8 rounded md:backdrop-blur-sm bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14'>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='relative mt-24 space-y-8 rounded md:backdrop-blur-sm bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14'
+      >
         <h1 className='text-4xl font-semibold'>Sign In</h1>
         <div className='space-y-4'>
           <label className='inline-block w-full'>
-            <input type='email' placeholder='email' className='input' />
+            <input
+              type='email'
+              placeholder='email'
+              className='input'
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <p className='p-1 text-[13px] font-light  text-orange-500'>
+                Please enter a valid email.
+              </p>
+            )}
           </label>
           <label className='inline-block w-full'>
-            <input type='password' placeholder='Password' className='input' />
+            <input
+              type='password'
+              placeholder='Password'
+              className='input'
+              {...register('password', { required: true })}
+            />
+            {errors.password && (
+              <p className='p-1 text-[13px] font-light  text-orange-500'>
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
-        <button className='w-full rounded bg-[#e50914] py-3 font-semibold'>
+        <button
+          type='submit'
+          className='w-full rounded bg-[#e50914] py-3 font-semibold'
+          onClick={() => setLogin(true)}
+        >
           Sign In
         </button>
         <div className='text-[gray]'>
@@ -38,6 +90,7 @@ function Login() {
           <button
             className='cursor-pointer text-white hover:underline'
             type='submit'
+            onClick={() => setLogin(false)}
           >
             Sign up now
           </button>
