e3c00e96ef7ee1e3054756cd6957d2980dc3ea03
Matthew Fogarty <mattempyre@gmail.com>
2022-10-30 22:54:33 +0100

    Finished Modal UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { modalState } from '../atoms/modalAtom';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 import Row from '../components/Row';
 import useAuth from '../hooks/useAuth';
 import { Movie } from '../typings';
 import requests from '../utils/requests';
+import { useRecoilValue } from 'recoil';
+import Modal from '../components/Modal';
 
 interface Props {
   netflixOriginals: Movie[];
@@ -30,10 +33,16 @@ const Home = ({
 }: Props) => {
   const { loading } = useAuth();
 
+  const showModal = useRecoilValue(modalState);
+
   if (loading) return null;
 
   return (
-    <div className='relative h-screen bg-gradient-to-b lg:h-[140vh]'>
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel='icon' href='/favicon.ico' />
@@ -51,7 +60,7 @@ const Home = ({
           <Row title='Romance Movies' movies={romanceMovies} />
           <Row title='Documentaries' movies={documentaries} />
         </section>
-        {/* Model */}
+        {showModal && <Modal />}
       </main>
     </div>
   );
